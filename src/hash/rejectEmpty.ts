84908205ff463192ec2dd960a49bca56dd362e50
absolute-boi <diego@diegoenriquezserrano.dev>
2023-03-10 20:55:41 -0600

    return false values in rejectEmpty

--- src/hash/rejectEmpty.ts
+++ src/hash/rejectEmpty.ts
@@ -2,6 +2,6 @@ export const rejectEmpty = <T>(
   object: Record<string | number | symbol, T>
 ): { [key: string]: T } => {
   return Object.fromEntries(
-    Object.entries(object).filter(([_key, value]) => !!value)
+    Object.entries(object).filter(([_key, value]) => (!!value || value === false))
   );
 };
